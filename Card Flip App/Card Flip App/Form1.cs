95c0f015270afde01cb712dfe21b33ba05af4261
Cey Bozz <91478796+ceybozz@users.noreply.github.com>
2022-07-12 23:35:42 +0200

    Add files via upload

---
+++ Card Flip App/Card Flip App/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Card_Flip_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showBackButton_Click(object sender, EventArgs e)
+        {
+            pictureBoxBack.Visible = true;
+            pictureBoxFront.Visible = false;
+        }
+
+        private void showFrontButton_Click(object sender, EventArgs e)
+        {
+            pictureBoxBack.Visible = false;
+            pictureBoxFront.Visible = true;
+        }
+    }
+}
