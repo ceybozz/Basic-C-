fc1199152a996b215bc391fd17cd540b28d6d169
Cey Bozz <91478796+ceybozz@users.noreply.github.com>
2022-07-12 23:36:45 +0200

    Add files via upload

---
+++ Flags App/Flags App/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flags_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void francePictureBox_Click(object sender, EventArgs e)
+        {
+            displayLabel.Text = "France";
+        }
+
+        private void germanPictureBox_Click(object sender, EventArgs e)
+        {
+            displayLabel.Text = "Germany";
+        }
+
+        private void finlandPictureBox_Click(object sender, EventArgs e)
+        {
+            displayLabel.Text = "Finland";
+        }
+    }
+}
